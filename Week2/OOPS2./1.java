aadd0e17d6ca5c7865c8e7e3d8613531ad49021d
PIYUSH PAL <piyush.pal8707@gmail.com>
2026-06-12 22:00:44 +0530

    Create 1.java

---
+++ Week2/OOPS2./1.java
@@ -0,0 +1,13 @@
+interface test{
+    int square(int n);
+}
+class Arithmetic implements test{
+    public int square(int n)  {
+        return n*n;
+    }}
+class ToTestInt{
+    public static void main(String args[]){
+        Arithmetic a = new Arithmetic();
+        System.out.println("Square = " + a.square(5));
+    }
+}
