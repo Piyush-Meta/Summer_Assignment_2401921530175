17cc440cd8f9a6c4530541087f1b7e612614c007
PIYUSH PAL <piyush.pal8707@gmail.com>
2026-06-12 22:03:31 +0530

    Create 3.java

---
+++ Week2/OOPS2./3.java
@@ -0,0 +1,31 @@
+class Point{
+    private int x;
+  private int y;
+ Point(){
+        x = 0;
+      y = 0;
+    }
+Point(int x,int y)    {
+        this.x = x;
+        this.y = y;
+    }
+void setX(int x)    {
+        this.x = x;
+    }
+    void setY(int y){
+               this.y = y;
+    }
+    void setXY(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+void show()    {
+  System.out.println("x = "+x);
+  System.out.println("y = "+y);
+  }
+    public static void main(String args[]) {
+        Point p = new Point();
+        p.setXY(10,20);
+        p.show();
+    }
+}
