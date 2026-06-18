d779814070592408f4d7fc27e4276617a0046ab9
PIYUSH PAL <piyush.pal8707@gmail.com>
2026-06-18 23:37:34 +0530

    Create polishNotation.java

---
+++ Week3/Day4/polishNotation.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for (String s : tokens) {
+      if (s.equals("+")) {
+                     int b = st.pop() ,a = st.pop();
+                 st.push(a + b);
+            }     
+else if (s.equals("-")) {
+       int b = st.pop(), a = st.pop();
+             st.push(a - b);
+            }
+else if (s.equals("*")) {
+            int b = st.pop(), a = st.pop();
+          st.push(a * b);
+            }
+     else if (s.equals("/")) {
+               int b = st.pop(), a = st.pop();
+                st.push(a / b);
+            }
+   else {            st.push(Integer.parseInt(s));
+       }
+        }
+        return st.pop();
+    }
+}
